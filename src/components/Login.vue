2e5537da43defb0fa18573220b7a82c87d98504e
郭燕超 <992410364@qq.com>
2022-02-07 00:08:59 +0800

    login static

---
+++ src/components/Login.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="login-container">
+        <!-- 头像区 -->
+        <div class="login-box">
+            <div class="avatar-box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+            <!-- 登录表单区 -->
+            <div class="form">
+                <el-form :model="loginForm" ref="loginFormRef" :rules="loginFormRules">
+                    <el-form-item prop="username">
+                        <el-input v-model="loginForm.username" prefix-icon="iconfont icon-yonghu"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="password">
+                        <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-mima"></el-input>
+                    </el-form-item>
+                    <el-form-item class="btns">
+                        <el-button type="primary">登录</el-button>
+                        <el-button type="info" @click="resetLoginForm">重置</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            // 登录表单数据绑定对象
+            loginForm:{
+                username:'',
+                password:''
+            },
+            loginFormRules:{
+                username:[
+                    {required:true,message:'请输入用户名',trigger:'blur'},
+                    {min:2,max:10,message:'长度为2-10个字符',trigger:'blur'}
+                ],
+                password:[
+                    {required:true,message:'请输入密码',trigger:'blur'},
+                    {min:6,max:15,message:'长度为6-15个字符',trigger:'blur'}
+
+                ]
+            }
+        }
+    },
+    methods:{
+        resetLoginForm(){
+            console.log(this)
+            this.$refs.loginFormRef.resetFields()
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.login-container{
+    height: 100%;
+    background: rgb(55, 150, 238);
+    .login-box{
+        width: 450px;
+        height: 300px;
+        background-color: #fff;
+        border-radius: 3px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+        .avatar-box{
+            height: 130px;
+            width: 130px;
+            border: 1px solid #aaa;
+            border-radius: 50%;
+            padding: 10px;
+            box-shadow: 0 0 10px #ddd;
+            position: absolute;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            background-color: white;
+            img{
+                height: 100%;
+                width: 100%;
+                border-radius: 50%;
+                background-color: #ddd;
+            }
+        }
+        .form{
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            padding: 0 10px;
+            box-sizing: border-box;
+            .btns{
+                display: flex;
+                justify-content: end;
+            }
+        }
+        
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入全局样式
+import './assets/css/global.css'
+//引入elementui
+import { Button, Form,FormItem,Input } from 'element-ui';
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
-
+Vue.use(Button);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
 new Vue({
   router,
   render: h => h(App)
